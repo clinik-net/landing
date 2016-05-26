15012b28420fb9838e9d3fd4f5cb2810c07ac2b9
Jonathan Leon <jonathan@cloudea.me>
2016-05-26 00:01:08 -0500

    template applied

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -22,6 +22,16 @@ return array(
                     ),
                 ),
             ),
+            'signup' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/sign-up',
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'signup',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
